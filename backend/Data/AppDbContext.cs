7b18a89e9eab0c706f9c3047518531a38bf9a293
l-library <borui-li@outlook.com>
2026-07-13 16:18:48 +0800

    Import scalar

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -7,4 +7,9 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("APP_OWNER");
+    }
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ShowtimeBackend.Data;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
